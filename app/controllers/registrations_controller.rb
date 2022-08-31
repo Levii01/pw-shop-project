b4527986683d67b3802f41007ada2d921d919b13
Miłosz Sędziak <milosz.sedziak@siepomaga.pl>
2022-09-01 01:32:42 +0200

    Refactor and validation for fields

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,13 +3,13 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def update_resource(resource, params)
-    if params[:password].blank?
-      params.delete(:password)
-      params.delete(:password_confirmation) if params[:password_confirmation].blank?
-      params.delete(:current_password) if params[:current_password].blank?
-    end
+  # def update_resource(resource, params)
+  #   if params[:password].blank?
+  #     params.delete(:password)
+  #     params.delete(:password_confirmation) if params[:password_confirmation].blank?
+  #     params.delete(:current_password) if params[:current_password].blank?
+  #   end
 
-    resource.update_without_password(params)
-  end
+  #   resource.update_without_password(params)
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,17 @@ class User < ApplicationRecord
                'Rolnictwo', 'Instrumenty'].freeze
 
   enum education: EDUCATION
+
+  strip_attributes only: %i[first_name last_name country city street aparment_no], collapse_spaces: true
+
+  validates :first_name, presence: true, on: :update
+  validates :last_name, presence: true, on: :update
+  validates :login, presence: true, on: :update
+  validates :country, presence: true, on: :update
+  validates :zip_code, presence: true, on: :update
+  validates :city, presence: true, on: :update
+  validates :street, presence: true, on: :update
+  validates :aparment_no, presence: true, on: :update
+  validates :education, presence: true, on: :update
+  validates :interests, presence: true, on: :update
 end
