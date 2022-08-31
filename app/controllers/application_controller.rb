42021671bd01bf25075ae6a0166aef9861b008dc
Miłosz Sędziak <milosz.sedziak@siepomaga.pl>
2022-09-01 00:47:37 +0200

    Update profile without current password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  USER_FIELDS = %i[first_name last_name login country zip_code city street aparment_no education
+                   interests].freeze
+  protect_from_forgery with: :exception
+
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password, *USER_FIELDS)
+    end
+  end
 end
