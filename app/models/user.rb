2461363b6e00a4d1bcf48b412bc19a8fb45e6ec3
Miłosz Sędziak <milosz.sedziak@siepomaga.pl>
2022-09-01 00:12:59 +0200

    Education enum and select

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  EDUCATION = { podstawowe: 0, średnie: 1, wyższe: 2 }.freeze
+
+  enum education: EDUCATION
 end
