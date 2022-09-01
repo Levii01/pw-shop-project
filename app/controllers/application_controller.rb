3ca7bfa6b965d98a866fca7a02d0253ccfc4ae23
Miłosz Sędziak <milosz.sedziak@siepomaga.pl>
2022-09-01 21:34:38 +0200

    Initialize carts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :cart
+
+  def cart
+    @cart ||= Cart.find_by(id: session[:cart_id]) || create_card
+  end
 
   protected
+
+  def create_card
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
+    @cart
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
